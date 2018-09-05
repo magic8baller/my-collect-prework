1e82aceeca1718fdb0e048f5fe04c928ce5a8df9
Leslie Turis <lturis@gmail.com>
2018-09-05 20:21:48 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i])
+    i += 1
+  end
+  array
+end
 
+array1.new_array { |item| item.split(' ').first }
+my_collect(array2) { |item| item.upcase }
